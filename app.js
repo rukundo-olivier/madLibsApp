800081ae631856fe3cedca3f8608a338820a224f
rukundo-olivier <vierukundo@gmail.com>
2022-04-23 07:51:43 +0200

    Added mad-lib replacement variables to story

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+console.log("hello");
+
+
+let userInputs = ["timid", "unexpectedly", "fright and haste", "Roused", "repay", 
+"generous ", "stalking", "tangles","struggling","gnawed",];
+
+let originalStory = `
+THE LION AND THE MOUSE  from Aesop’s Fables 
+
+A Lion lay asleep in the forest, his great head resting on his paws.
+A <h2>${userInputs[0]}</h2> little Mouse came upon him <h2>${userInputs[1]}</h2>, and in her <h2>${userInputs[2]}</h2> to get away, ran across the Lion's nose. 
+<h2>${userInputs[3]}</h2> from his nap, the Lion laid his huge paw angrily on the tiny creature to kill her. 
+"Spare me!" begged the poor Mouse. "Please let me go and someday I will surely <h2>${userInputs[4]}</h2> you."  
+The Lion was much amused to think that a Mouse could ever help him. But he was <h2>${userInputs[5]}</h2> and finally let the Mouse go. 
+Some days later, while <h2>${userInputs[6]}</h2> his prey in the forest, the Lion was caught in the <h2>${userInputs[7]}</h2> of a hunter's net. 
+Unable to free himself, he filled the forest with his angry roaring. 
+The Mouse knew the voice and quickly found the Lion <h2>${userInputs[8]}</h2> in the net. 
+Running to one of the great ropes that bound him, she <h2>${userInputs[9]}</h2> it until it split apart, and soon the Lion was free. 
+"You laughed when I said I would repay you," said the Mouse. "Now you see that even a Mouse can help a Lion." 
+
+A kindness is never wasted.  
+ `;
+ console.log(originalStory);
+
+
+
+
+ /* THE LION AND THE MOUSE  from Aesop’s Fables 
+
+ A Lion lay asleep in the forest, his great head resting on his paws.
+ A timid little Mouse came upon him unexpectedly, and in her fright and haste to get away, ran across the Lion's nose. 
+ Roused from his nap, the Lion laid his huge paw angrily on the tiny creature to kill her. 
+ "Spare me!" begged the poor Mouse. "Please let me go and someday I will surely repay you."  
+ The Lion was much amused to think that a Mouse could ever help him. But he was generous and finally let the Mouse go. 
+ Some days later, while stalking his prey in the forest, the Lion was caught in the tangles of a hunter's net. 
+ Unable to free himself, he filled the forest with his angry roaring. 
+ The Mouse knew the voice and quickly found the Lion struggling in the net. 
+ Running to one of the great ropes that bound him, she gnawed it until it split apart, and soon the Lion was free. 
+ "You laughed when I said I would repay you," said the Mouse. "Now you see that even a Mouse can help a Lion." 
+ 
+ A kindness is never wasted.  
+  */
